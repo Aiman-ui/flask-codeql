b02b87215fe4f041dd9aed9a77f9a4d8d354f891
Aiman Zafar <aniqaz456@gmail.com>
2025-06-05 11:57:28 +0500

    Update vulnerable_demo.py

--- vulnerable_demo.py
+++ vulnerable_demo.py
@@ -8,7 +8,7 @@ os.system("echo " + user_input)
 
 # 2. Subprocess injection
 subprocess.run("ls " + user_input, shell=True)
-
+  
 # 3. Unsafe deserialization
 data = b"cos\nsystem\n(S'echo vulnerable'\ntR."
 pickle.loads(data)
