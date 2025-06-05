9f37a5f6640c6ca97b733034865f295cae70c511
Aiman Zafar <aniqaz456@gmail.com>
2025-06-05 11:44:01 +0500

    added file with security issue

---
+++ vulnerable_demo.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import pickle
+
+# 1. Command Injection
+user_input = input("Enter something to run: ")
+os.system("echo " + user_input)
+
+# 2. Subprocess injection
+subprocess.run("ls " + user_input, shell=True)
+
+# 3. Unsafe deserialization
+data = b"cos\nsystem\n(S'echo vulnerable'\ntR."
+pickle.loads(data)
+
